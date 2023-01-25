52256336dbc4ffe8319be502e94be63e8f0d9b61
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2023-01-25 16:41:27 +0000

    Initial Commit

---
+++ Now Boarding/Now_BoardingApp.swift
@@ -0,0 +1,20 @@
+//
+//  Now_BoardingApp.swift
+//  Now Boarding
+//
+//  Created by Peter Cole on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Now_BoardingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
